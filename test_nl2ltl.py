fa20bb7bc32c45059cf2cb4d62f71c810fa0c7d6
Emi Soroka <emiko@me.com>
2026-02-17 16:30:51 -0800

    Successfully run nl2ltl test script

---
+++ test_nl2ltl.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("nl2ltl")
+from dotenv import load_dotenv
+load_dotenv()
+
+from nl2ltl import translate
+from nl2ltl.engines.gpt.core import GPTEngine, Models
+from nl2ltl.filters.simple_filters import BasicFilter
+from nl2ltl.engines.utils import pretty
+
+engine = GPTEngine()
+filter = BasicFilter()
+utterance = "Eventually send me a Slack after receiving a Gmail"
+
+ltlf_formulas = translate(utterance, engine, filter)
+pretty(ltlf_formulas)
